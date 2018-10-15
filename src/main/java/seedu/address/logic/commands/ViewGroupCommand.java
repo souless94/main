2d9991ba5f524a0c6be345fbfad2b58c9d59b67f
Happytreat <cowfats@gmail.com>
2018-10-15 22:43:51 +0800

    Edit ViewGroupCommand to list group members in PersonPanel

--- src/main/java/seedu/address/logic/commands/ViewGroupCommand.java
+++ src/main/java/seedu/address/logic/commands/ViewGroupCommand.java
@@ -4,11 +4,14 @@ import static java.util.Objects.requireNonNull;
 import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
 import static seedu.address.model.Model.PREDICATE_SHOW_ALL_GROUPS;
 
+import java.util.function.Predicate;
+
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
 import seedu.address.model.Model;
 import seedu.address.model.group.Group;
 import seedu.address.model.person.Name;
+import seedu.address.model.person.Person;
 
 /**
  * Lists all members of the group entered to the user.
@@ -39,7 +42,10 @@ public class ViewGroupCommand extends Command {
         requireNonNull(model);
         Group group = CommandUtil.retrieveGroupFromIndex(model, groupName);
 
+        Predicate<Person> predicateShowAllGroupMembers = person -> group.getGroupMembers().contains(person);
+
         model.updateFilteredGroupList(PREDICATE_SHOW_ALL_GROUPS);
-        return new CommandResult(MESSAGE_SUCCESS + group.printMembers());
+        model.updateFilteredPersonList(predicateShowAllGroupMembers);
+        return new CommandResult(MESSAGE_SUCCESS);
     }
 }
