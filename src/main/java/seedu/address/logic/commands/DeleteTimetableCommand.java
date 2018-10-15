f85912b2032441985b6f90e715fc7dc0e2d35795
Happytreat <cowfats@gmail.com>
2018-10-15 16:09:20 +0800

    Edit for travis checkstyle

--- src/main/java/seedu/address/logic/commands/DeleteTimetableCommand.java
+++ src/main/java/seedu/address/logic/commands/DeleteTimetableCommand.java
@@ -5,10 +5,8 @@ import static seedu.address.model.Model.PREDICATE_SHOW_ALL_PERSONS;
 
 import java.io.File;
 
-import java.util.List;
 import java.util.Set;
 
-import seedu.address.commons.core.Messages;
 import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.logic.commands.exceptions.CommandException;
