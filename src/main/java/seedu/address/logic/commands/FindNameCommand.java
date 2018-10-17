cc9999f9f6086665d74ac372d613d8c6e09cb8ec
Zhi Wei <projects.zw@gmail.com>
2018-10-17 23:10:37 +0800

    Edit Code Style

--- src/main/java/seedu/address/logic/commands/FindNameCommand.java
+++ src/main/java/seedu/address/logic/commands/FindNameCommand.java
@@ -1,12 +1,12 @@
 package seedu.address.logic.commands;
 
+import static java.util.Objects.requireNonNull;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
 import seedu.address.model.person.Person;
-
-import static java.util.Objects.requireNonNull;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
