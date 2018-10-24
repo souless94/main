f0c3f29b6acb45d8c3480a3e173f97afe77ef4b6
Happytreat <cowfats@gmail.com>
2018-10-24 21:27:15 +0800

    Edit for travis checkstyle

--- src/test/java/seedu/address/logic/commands/AddGroupCommandIntegrationTest.java
+++ src/test/java/seedu/address/logic/commands/AddGroupCommandIntegrationTest.java
@@ -12,9 +12,7 @@ import seedu.address.model.Model;
 import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.group.Group;
-import seedu.address.model.person.Person;
 import seedu.address.testutil.GroupBuilder;
-import seedu.address.testutil.PersonBuilder;
 
 /**
  * Contains integration tests (interaction with the Model) for {@code AddCommand}.
