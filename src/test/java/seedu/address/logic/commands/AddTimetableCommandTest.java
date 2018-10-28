e5caa6f1e2b0a44c6478bcc3b4c240ff27309e27
souless94 <cl.wk@hotmail.com>
2018-10-28 11:22:36 +0800

    create more tests

--- src/test/java/seedu/address/logic/commands/AddTimetableCommandTest.java
+++ src/test/java/seedu/address/logic/commands/AddTimetableCommandTest.java
@@ -1,11 +1,16 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import org.junit.jupiter.api.Test;
+import java.io.File;
 
+import org.junit.Test;
+
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -13,20 +18,20 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 
-class AddTimetableCommandTest {
+public class AddTimetableCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
-
     @Test
-    void execute_timetableAcceptedByModel_addSuccess() {
+    public void execute_timetableAcceptedByModel_addSuccess() {
         Person personToAddTimetable = model.getFilteredPersonList()
             .get(INDEX_FIRST.getZeroBased());
         personToAddTimetable.getTimetable().downloadTimetableAsCsv();
-        AddTimetableCommand addTimetableCommand = new AddTimetableCommand(INDEX_FIRST, null);
+        AddTimetableCommand addTimetableCommand = new AddTimetableCommand(INDEX_FIRST, "default");
         String expectedMessage = String
-            .format(AddTimetableCommand.MESSAGE_ADD_TIMETABLE_SUCCESS, personToAddTimetable);
+            .format(AddTimetableCommand.MESSAGE_ADD_TIMETABLE_SUCCESS,
+                personToAddTimetable.getStoredLocation());
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
             new UserPrefs());
 
@@ -37,14 +42,15 @@ class AddTimetableCommandTest {
     }
 
     @Test
-    void execute_timetableAcceptedByModel_addByFileLocationSuccess() {
+    public void execute_timetableAcceptedByModel_addByFileLocationSuccess() {
         Person personToAddTimetable = model.getFilteredPersonList()
             .get(INDEX_FIRST.getZeroBased());
         personToAddTimetable.getTimetable().downloadTimetableAsCsv();
         AddTimetableCommand addTimetableCommand = new AddTimetableCommand(INDEX_FIRST,
             personToAddTimetable.getStoredLocation());
         String expectedMessage = String
-            .format(AddTimetableCommand.MESSAGE_ADD_TIMETABLE_SUCCESS, personToAddTimetable);
+            .format(AddTimetableCommand.MESSAGE_ADD_TIMETABLE_SUCCESS,
+                personToAddTimetable.getStoredLocation());
         Model expectedModel = new ModelManager(new AddressBook(model.getAddressBook()),
             new UserPrefs());
 
@@ -55,4 +61,27 @@ class AddTimetableCommandTest {
     }
 
 
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        AddTimetableCommand addTimetableCommand = new AddTimetableCommand(outOfBoundIndex,
+            "default");
+
+        assertCommandFailure(addTimetableCommand, model, commandHistory,
+            Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
+    }
+
+    @Test
+    public void execute_timetableNotFound_addFailure() {
+        Person personToAddTimetable = model.getFilteredPersonList()
+            .get(INDEX_FIRST.getZeroBased());
+        personToAddTimetable.getTimetable().downloadTimetableAsCsv();
+        File timetable = new File(personToAddTimetable.getStoredLocation());
+        if (timetable.exists()) {
+            timetable.delete();
+        }
+        AddTimetableCommand addTimetableCommand = new AddTimetableCommand(INDEX_FIRST, "default");
+        assertCommandFailure(addTimetableCommand, model, commandHistory,
+            Messages.MESSAGE_TIMETABLE_NOT_FOUND);
+    }
 }
--- src/test/java/seedu/address/logic/commands/EditTimetableCommandTest.java
+++ src/test/java/seedu/address/logic/commands/EditTimetableCommandTest.java
@@ -1,11 +1,14 @@
 package seedu.address.logic.commands;
 
+import static seedu.address.logic.commands.CommandTestUtil.assertCommandFailure;
 import static seedu.address.logic.commands.CommandTestUtil.assertCommandSuccess;
 import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 import static seedu.address.testutil.TypicalPersons.getTypicalAddressBook;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
 
+import seedu.address.commons.core.Messages;
+import seedu.address.commons.core.index.Index;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.AddressBook;
 import seedu.address.model.Model;
@@ -13,14 +16,13 @@ import seedu.address.model.ModelManager;
 import seedu.address.model.UserPrefs;
 import seedu.address.model.person.Person;
 
-class EditTimetableCommandTest {
-
+public class EditTimetableCommandTest {
 
     private Model model = new ModelManager(getTypicalAddressBook(), new UserPrefs());
     private CommandHistory commandHistory = new CommandHistory();
 
     @Test
-    void execute_timetableAcceptedByModel_editSuccess() {
+    public void execute_timetableAcceptedByModel_editSuccess() {
         Person personToEditTimetable = model.getFilteredPersonList()
             .get(INDEX_FIRST.getZeroBased());
         String day = "wednesday";
@@ -36,5 +38,18 @@ class EditTimetableCommandTest {
         expectedModel.commitAddressBook();
         assertCommandSuccess(editTimetableCommand, model, commandHistory, expectedMessage,
             expectedModel);
+    }
+
+    @Test
+    public void execute_invalidPersonIndexUnfilteredList_failure() {
+        Index outOfBoundIndex = Index.fromOneBased(model.getFilteredPersonList().size() + 1);
+        String day = "wednesday";
+        String timing = "0800";
+        String details = "test,magic try/.../";
+        EditTimetableCommand editTimetableCommand = new EditTimetableCommand(outOfBoundIndex, day,
+            timing, details);
+
+        assertCommandFailure(editTimetableCommand, model, commandHistory,
+            Messages.MESSAGE_INVALID_PERSON_DISPLAYED_INDEX);
     }
 }
