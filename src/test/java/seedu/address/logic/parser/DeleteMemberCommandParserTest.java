3fd181f347f44f6a05e4891546b5cf09e84df9a7
Happytreat <cowfats@gmail.com>
2018-10-30 18:39:58 +0800

    Edit for travis checkstyle

--- src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
+++ src/test/java/seedu/address/logic/parser/DeleteMemberCommandParserTest.java
@@ -5,26 +5,26 @@ import static seedu.address.logic.commands.CommandTestUtil.GROUPNAME_DESC_BOO;
 import static seedu.address.logic.commands.CommandTestUtil.VALID_GROUP_NAME_BOO;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseFailure;
 import static seedu.address.logic.parser.CommandParserTestUtil.assertParseSuccess;
+import static seedu.address.testutil.TypicalIndexes.INDEX_FIRST;
 
 import org.junit.Test;
 
-import seedu.address.logic.commands.DeleteGroupCommand;
-import seedu.address.model.group.Group;
-import seedu.address.testutil.GroupBuilder;
+import seedu.address.logic.commands.DeleteMemberCommand;
+import seedu.address.model.person.Name;
 
-public class DeleteGroupCommandParserTest {
+public class DeleteMemberCommandParserTest {
 
-    private DeleteGroupCommandParser parser = new DeleteGroupCommandParser();
+    private DeleteMemberCommandParser parser = new DeleteMemberCommandParser();
 
     @Test
-    public void parseValidArgsReturnsDeleteGroupCommand() {
-        Group group = new GroupBuilder().withName(VALID_GROUP_NAME_BOO).build();
-        assertParseSuccess(parser, GROUPNAME_DESC_BOO, new DeleteGroupCommand(group));
+    public void parseValidArgsReturnsDeleteMemberCommand() {
+        assertParseSuccess(parser, " 1" + GROUPNAME_DESC_BOO, new DeleteMemberCommand(
+                new Name(VALID_GROUP_NAME_BOO), INDEX_FIRST));
     }
 
     @Test
     public void parseInvalidArgsThrowsParseException() {
         assertParseFailure(parser, "a", String.format(MESSAGE_INVALID_COMMAND_FORMAT,
-                DeleteGroupCommand.MESSAGE_USAGE));
+                DeleteMemberCommand.MESSAGE_USAGE));
     }
 }
