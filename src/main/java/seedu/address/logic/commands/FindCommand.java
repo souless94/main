607d195b97fa95db10b405362846355efe789b79
Zhi Wei <projects.zw@gmail.com>
2018-10-31 19:56:58 +0800

    FindCommand: Prefixes Search

--- src/main/java/seedu/address/logic/commands/FindCommand.java
+++ src/main/java/seedu/address/logic/commands/FindCommand.java
@@ -2,17 +2,24 @@ package seedu.address.logic.commands;
 
 import static java.util.Objects.requireNonNull;
 
+import java.util.List;
+
 import seedu.address.commons.core.Messages;
 import seedu.address.logic.CommandHistory;
 import seedu.address.model.Model;
 import seedu.address.model.person.NameContainsKeywordsPredicate;
-import seedu.address.model.person.Person;
 
 /**
  * Finds and lists all persons in address book whose name contains any of the argument keywords.
  * Keyword matching is case insensitive.
  */
 public class FindCommand extends Command {
+
+    public final List<String> nameKeywords;
+    public final List<String> phoneKeywords;
+    public final List<String> addressKeywords;
+    public final List<String> emailKeywords;
+    public final List<String> tagKeywords;
 
     public static final String COMMAND_WORD = "find";
 
@@ -21,15 +28,20 @@ public class FindCommand extends Command {
             + "Parameters: KEYWORD [MORE_KEYWORDS]...\n"
             + "Example: " + COMMAND_WORD + " alex friends gardens";
 
-    private final NameContainsKeywordsPredicate<Person> predicate;
-
-    public FindCommand(NameContainsKeywordsPredicate<Person> predicate) {
-        this.predicate = predicate;
+    public FindCommand(List<String> nameKeywords, List<String> phoneKeywords, List<String> addressKeywords,
+                       List<String> emailKeywords, List<String> tagKeywords) {
+        this.nameKeywords = nameKeywords;
+        this.phoneKeywords = phoneKeywords;
+        this.addressKeywords = addressKeywords;
+        this.emailKeywords = emailKeywords;
+        this.tagKeywords = tagKeywords;
     }
 
     @Override
     public CommandResult execute(Model model, CommandHistory history) {
         requireNonNull(model);
+        NameContainsKeywordsPredicate predicate = new NameContainsKeywordsPredicate(
+                nameKeywords, addressKeywords, phoneKeywords, emailKeywords, tagKeywords, "all");
         model.updateFilteredPersonList(predicate);
         return new CommandResult(
                 String.format(Messages.MESSAGE_PERSONS_LISTED_OVERVIEW, model.getFilteredPersonList().size()));
@@ -39,6 +51,11 @@ public class FindCommand extends Command {
     public boolean equals(Object other) {
         return other == this // short circuit if same object
                 || (other instanceof FindCommand // instanceof handles nulls
-                && predicate.equals(((FindCommand) other).predicate)); // state check
+                && nameKeywords.equals(((FindCommand) other).nameKeywords) // state check
+                && phoneKeywords.equals(((FindCommand) other).phoneKeywords)
+                && addressKeywords.equals(((FindCommand) other).addressKeywords)
+                && emailKeywords.equals(((FindCommand) other).emailKeywords)
+                && tagKeywords.equals(((FindCommand) other).tagKeywords));
     }
+
 }
--- src/main/java/seedu/address/logic/parser/FindCommandParser.java
+++ src/main/java/seedu/address/logic/parser/FindCommandParser.java
@@ -1,12 +1,16 @@
 package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_ADDRESS;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_EMAIL;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_NAME;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_PHONE;
+import static seedu.address.logic.parser.CliSyntax.PREFIX_TAG;
 
 import java.util.Arrays;
 
 import seedu.address.logic.commands.FindCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.person.NameContainsKeywordsPredicate;
 
 /**
  * Parses input arguments and creates a new FindCommand object
@@ -25,8 +29,60 @@ public class FindCommandParser implements Parser<FindCommand> {
                     String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
         }
 
-        String[] nameKeywords = trimmedArgs.split("\\s+");
+        ArgumentMultimap argMultimap =
+                ArgumentTokenizer
+                        .tokenize(args, PREFIX_NAME, PREFIX_PHONE, PREFIX_EMAIL, PREFIX_ADDRESS,
+                                PREFIX_TAG);
 
-        return new FindCommand(new NameContainsKeywordsPredicate<>(Arrays.asList(nameKeywords), "all"));
+        if (!argMultimap.getPreamble().isEmpty()) {
+            throw new ParseException(
+                    String.format(MESSAGE_INVALID_COMMAND_FORMAT, FindCommand.MESSAGE_USAGE));
+        }
+
+        String name = argMultimap.getValue(PREFIX_NAME).orElse("");
+        String phone = argMultimap.getValue(PREFIX_PHONE).orElse("");
+        String email = argMultimap.getValue(PREFIX_EMAIL).orElse("");
+        String address = argMultimap.getValue(PREFIX_ADDRESS).orElse("");
+        String tag = argMultimap.getValue(PREFIX_TAG).orElse("");
+
+        String[] nameKeywords;
+        String[] phoneKeywords;
+        String[] addressKeywords;
+        String[] emailKeywords;
+        String[] tagKeywords;
+
+        if (!name.equals("")) {
+            nameKeywords = name.split("\\s+");
+        } else {
+            nameKeywords = new String[0];
+        }
+
+        if (!phone.equals("")) {
+            phoneKeywords = phone.split("\\s+");
+        } else {
+            phoneKeywords = new String[0];
+        }
+
+        if (!address.equals("")) {
+            addressKeywords = address.split("\\s+");
+        } else {
+            addressKeywords = new String[0];
+        }
+
+        if (!email.equals("")) {
+            emailKeywords = email.split("\\s+");
+        } else {
+            emailKeywords = new String[0];
+        }
+
+        if (!tag.equals("")) {
+            tagKeywords = tag.split("\\s+");
+        } else {
+            tagKeywords = new String[0];
+        }
+
+        return new FindCommand(Arrays.asList(nameKeywords), Arrays.asList(phoneKeywords),
+                Arrays.asList(addressKeywords), Arrays.asList(emailKeywords), Arrays.asList(tagKeywords));
     }
+
 }
