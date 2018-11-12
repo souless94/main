7a16bc0ac3e3aa765a6046c1cabce2eeb4dac14a
souless94 <cl.wk@hotmail.com>
2018-11-12 11:31:26 +0800

    fix timetableDataTest having wrong test case

--- src/test/java/seedu/address/model/person/timetable/TimetableDataTest.java
+++ src/test/java/seedu/address/model/person/timetable/TimetableDataTest.java
@@ -17,10 +17,10 @@ public class TimetableDataTest {
         BOB.getStoredLocation(),
         "0700,800,900,1000," + timetableString, 1, null, null, null);
 
-    private String wrongTimetableString =
-        "aG9yaXpvbnRhbA==,aG9yaXpvbnRhbA==," + timetableString.substring(5);
+    private String wrongTimetableRowString =
+        "aG9yaXpvbnRhbA==,aG9yaXpvbnRhbA==," + timetableString.substring(14);
     private TimetableData timetableDataWrongFirstRow = new TimetableData(
-        BOB.getStoredLocation(), wrongTimetableString, 1, null, null,
+        BOB.getStoredLocation(), wrongTimetableRowString, 1, null, null,
         null);
 
 
