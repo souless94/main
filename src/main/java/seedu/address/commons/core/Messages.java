87e0f8f99d825b67df2b15787753a42f11cc06a7
aspiringdevslog <aspiringdevslog@gmail.com>
2018-11-12 16:36:04 +0800

    fix trailing whitespace

--- src/main/java/seedu/address/commons/core/Messages.java
+++ src/main/java/seedu/address/commons/core/Messages.java
@@ -24,6 +24,5 @@ public class Messages {
         "Admin rights required to create an account. " +
             "Please login to admin account or contact an admin to continue. " +
             "Refer to User Guide section 5.1 for details";
-    
 
 }
